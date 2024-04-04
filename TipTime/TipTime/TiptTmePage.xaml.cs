2f5c41353e387e15ae7294104fbfc989f22f8f84
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2024-04-04 12:36:08 -0300

    feat: adicionei a interface e inciei a logica

--- TipTime/TipTime/TiptTmePage.xaml.cs
+++ TipTime/TipTime/TiptTmePage.xaml.cs
@@ -7,4 +7,41 @@ public partial class TiptTmePage : ContentPage
 		InitializeComponent();
 	}
 
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+
+
+
+
+    private void arredDown_Clicked(object sender, EventArgs e)
+    {
+        tipsPerSlider.Value = tipsPerSlider.Value - 1;
+    }
+
+
+    private void arredUp_Clicked(object sender, EventArgs e)
+    {
+        tipsPerSlider.Value = tipsPerSlider.Value + 1;
+    }
+
+    private void Percentage15Button_Clicked(object sender, EventArgs e)
+    {
+        tipsPerSlider.Value = 15;
+    }
+
+    private void Percentage30Button_Clicked(object sender, EventArgs e)
+    {
+        tipsPerSlider.Value = 30;
+    }
+
+    private void tipsPerSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        double valorDoSLider = tipsPerSlider.Value;
+        sliderPer.Text = valorDoSLider.ToString() + "%";
+    
+    
+    }
 }
